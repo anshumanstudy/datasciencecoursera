8907ddf9c2de64b40b475404994c1da749e16d96
anshumanstudy <anshumanch13@gmail.com>
2017-06-11 19:13:32 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,9 @@
 
 ## Write a short comment describing this function
 
+## The function makeCacheMatrix() created a special "matrix" object and enables
+## caching of its inverse. It builds a list of functions to perform this.
+
 makeCacheMatrix <- function(x = matrix()) {
    
      m <- NULL
@@ -23,19 +26,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## The function cacheSolve() calculates the inverse of the special "matrix"
+## from function makeCacheMatrix().  It also ensures that if the inverse of the
+## matrix is present in the cache then skips the calculation and pulls the 
+## result from the cache. If the calculation of the inverse is NEW then, it 
+## calculates and puts it in the cache.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     
     m <- x$getinv()
     
-    if(!is.null(m)){
+    if(!is.null(m)){  ## check if inverse is already in the cache
         message("getting cached data...")
         return(m)
     }
     
     data <- x$get()
     
-    if (nrow(data) == ncol(data)) {
+    ## checking if it is a square matrix and numeric to calculate its inverse
+    
+    if (nrow(data) == ncol(data)) {  
         if (is.numeric(data)) {
             m <- x$setinv(data)
         } else { print("not a numeric matrix") }
